446beb3005f822e9c1747e2296d2d5f322d76e39
ishanEPR <reshmikaediriweera1997@gmail.com>
2021-05-02 21:23:50 +0530

    add database connect files

--- src/codes/DBconnect.java
+++ src/codes/DBconnect.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package codes;
 
-/**
- *
- * @author ASUS
- */
+import com.sun.jdi.connect.spi.Connection;
+
+
+
 public class DBconnect {
+    public static Connection connect(){
+        Connection conn=null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+           // conn=DriverManager.getConnection(null);
+            
+        }catch(Exception e){
+            
+        }
+        
+        return conn;
+    }
     
 }
